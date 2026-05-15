e9966beeca038d162b78351a184691e3e48cd513
srilatha2012 <srilathp2012@gmail.com>
2026-05-15 18:43:14 -0400

    Created routes to fetch specific movie details

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,1 +1,21 @@
-const router = express.Router();
+//Import express
+const express = require("express");
+
+//create a new router object for movie routes
+const movieRouter = express.Router();
+
+//Import movie controller functions
+const movieController = require("../controllers/movieController");
+
+//Route to fetch details for a specific movie using movie id
+movieRouter.get("/movies/:id" , (req,res) => {
+    res.send(`Movie 1`);
+});
+
+//Route to handle movie search requests
+movieRouter.get("/search", (req,res) =>{
+   res.send(`Movie 2`);
+});
+
+//Export the router
+module.exports =movieRouter
